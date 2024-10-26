9aee68d200bd3cd78c1be85d43d280a74f010643
Karam_bola <dmitriy_tom@tut.by>
2024-10-27 01:14:45 +0300

    Добавьте файлы проекта.

---
+++ View/AddDisciplineWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PspClient.View
+{
+    public partial class AddDisciplineWindow : Window
+    {
+        public AddDisciplineWindow()
+        {
+            InitializeComponent();
+        
+        }
+        private async void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NameTextBox.Text))
+            {
+                MessageBox.Show("Проверьте правильность введенных данных.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var discipline = new
+            {
+                Name = NameTextBox.Text,
+            };
+
+            // Отправка данных на сервер
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7008/");
+                    var json = JsonConvert.SerializeObject(discipline);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    var response = await client.PostAsync("api/Discipline/add", content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Дисциплина успешно добавлена.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                        this.Close();
+                    }
+                    else
+                    {
+                        var errorMessage = await response.Content.ReadAsStringAsync();
+                        MessageBox.Show($"Не удалось добавить дисциплину: {errorMessage}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ View/DeleteGradeWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PspClient.View
+{
+    /// <summary>
+    /// Логика взаимодействия для DeleteGradeWindow.xaml
+    /// </summary>
+    public partial class DeleteGradeWindow : Window
+    {
+        public DeleteGradeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7008/");
+                    if (int.TryParse(DeleteGradeIDInput.Text, out int GradeID))
+                    {
+                        var response = await client.DeleteAsync($"https://localhost:7008/api/Grade/delete/{GradeID}");
+
+                        if (response.IsSuccessStatusCode)
+                            MessageBox.Show("Оценка успешно удалена.");
+                        else
+                        {
+                            string errorMessage = await response.Content.ReadAsStringAsync();
+                            MessageBox.Show($"Ошибка удаления оценки: {errorMessage}");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введите корректный ID оценки для удаления.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
